4480838525405077e39f9795bd7c0dd66e8ced79
Zahnschmelz <68254641+Zahnschmelz@users.noreply.github.com>
2026-03-07 07:19:46 +0100

    Increase resolution and adjust color iteration logic

--- mandelbrot.py
+++ mandelbrot.py
@@ -6,7 +6,7 @@ import math
 pygame.init()
 
 # Bildschirmgröße und -einstellungen
-WIDTH, HEIGHT = 400, 300
+WIDTH, HEIGHT = 768, 512
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Mandelbrot Map mit Farbwechsel")
 
@@ -28,15 +28,15 @@ def mandelbrot(c, max_iter):
 def main():
     clock = pygame.time.Clock()
     running = True
-    max_iter = 15 #256
+    max_iter = 25 #256
     zoom = 0.5
     offset_x, offset_y = -0.5, 0.0
-    iteration_count = 0
+    iteration_count = 1
 
     # RGB Startbereich
 
-    r = 127
-    g = 0
+    r = 0
+    g = 127
     b = 64
 
     #check RGB
@@ -54,30 +54,30 @@ def main():
 
         # Farbwechsel (in der Menge)
         if r < 255 and checkr == False:
-            r = r + 2
-        elif r <= 255:
+            r = r + 3
+        elif r >= 255:
             checkr = True
         if checkr == True:
             r = r - 2
-        if r == 0:
+        if r <= 0:
             checkr = False
 
         if g < 255 and checkg == False:
-            g = g + 1
+            g = g + 2
         elif g >= 255:
             checkg = True
         if checkg == True:
-            g = g - 5
-        if g == 0:
+            g = g - 4
+        if g <= 0:
             checkg = False
 
         if b < 255 and checkb == False:
-            b = b + 10
+            b = b + 8
         elif b >= 255:
             checkb = True
         if checkb == True:
             b = b - 1
-        if b == 0:
+        if b <= 0:
             checkb = False
 
         # Überlauf
@@ -87,14 +87,19 @@ def main():
             r = 0
         if g > 255:
             g = 255
-        if r < 0:
-            r = 0
+        if g < 0:
+            g = 0
         if b > 255:
             b = 255
-        if r < 0:
-            r = 0
+        if b < 0:
+            b = 0
 
-        screen.fill((0, 0, 0)) # Schwarz für Punkte außerhalb der Menge
+        print(checkr)
+        print(checkg)
+        print(checkb)
+
+
+        screen.fill((r, g, b)) # Schwarz für Punkte außerhalb der Menge
 
         # Zeichnen der Mandelbrot-Menge
         for x in range(WIDTH):
@@ -115,9 +120,15 @@ def main():
                 screen.set_at((x, y), color)
 
         pygame.display.flip()
+        print("ticktack")
         clock.tick(60)  # 60 FPS
+        print(color)
+        print(r,g,b)
     pygame.quit()
     sys.exit()
+
+if __name__ == "__main__":
+    main()
 
 if __name__ == "__main__":
     main()
